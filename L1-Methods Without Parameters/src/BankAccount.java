42c3cd54c5c8a714e2b4ff5c3c7220971597290b
adamsjer000 <adamsjer000@warren.k12.in.us>
2021-09-16 12:49:17 -0400

    Important

--- L1-Methods Without Parameters/src/BankAccount.java
+++ L1-Methods Without Parameters/src/BankAccount.java
@@ -1,18 +1,39 @@
-public class BankAccount {
+import javax.swing.*;
+
+public class BankAccount<balance, withdrawal> {
 
     //create a program which simulates a bank account
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-    }
 
+    deposit();
+
+    withdrawal();
+}
     /*write a method to simulate a deposit.
     Prompt the user to enter their balance and the deposit amount,
     then display the new balance. */
 
+    public static void deposit(){
+
+        double balance =  Double.parseDouble(JOptionPane.showInputDialog("What is your current balance ?")
+        double deposit = Double.parseDouble(JOptionPane.showInputDialog("What is your deposit ?");
+
+        balance += deposit;
+
+        JOptionPane.showMessageDialog(null, "Your new balance is:" + balance);
+    }
 
     /*write a method to simulate a withdrawal.
     Prompt the user to enter their balance the withdrawal amount,
     then display the new balance.
      */
+public static void withdrawal(){
+
+}
+    double balance = Double.parseDouble(JOptionPane.showInputDialog("What is your current balance ?"));
+    double withdrawal = Double.parseDouble(JOptionPane.showInputDialog("What is your withdrawal amount ?"));
+
+    balance -= withdrawal;
 }
--- L3-Return Methods/src/CircleCalc.java
+++ L3-Return Methods/src/CircleCalc.java
@@ -1,15 +1,64 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class CircleCalc {
 
 
     public static void main(String[] args) {
 
-        //create and run the program
+        double radius = getInput("What is the radius?");
 
+        double area = area(radius);
+
+        double circumference = circumference(radius);
+
+        //JOptionPane.showMessageDialog(null, displayResult(area, circumference));
+        outputResults(area, circumference);
     }
 
+
+        //create and run the program
+
+
+
+    //Get the input
+    public static double getInput(String prompt){
+        return Double.parseDouble(JOptionPane.showInputDialog(prompt));
+
+
+    }
     //re-write the method to calculate a circle's area using parameters and return statement
 
+      public static double area (double radius) {
+
+          return Math.pow(radius, 2) * Math.PI;
+      }
 
     //re-write the method to calculate a circle's perimeter using parameters and return statement
+      public static double circumference(double radius){
 
+          return 2 * Math.PI * radius;
+      }
+
+
+
+      public static String displayResults(double area, double circumference){
+
+          DecimalFormat round = new DecimalFormat("#.00");
+
+        String message = "The area is:" + area;
+        message += "\nThe circumference is:" + round.format (circumference);
+
+        return message;
+      }
+
+      public static void outputResults(double area, double circumference){
+          DecimalFormat round = new DecimalFormat("#.00");
+
+          String message = "The area is:" + area;
+          message += "\nThe circumference is:" + round.format (circumference);
+
+          JOptionPane.showMessageDialog(null, message);
+
+      }
 }
