c023134b0c7c3c483411a37349619de818d8556a
adamsjer000 <adamsjer000@warren.k12.in.us>
2021-09-28 13:10:07 -0400

    Important

--- L5-Swing Frames and Buttons/src/Quotes.java
+++ L5-Swing Frames and Buttons/src/Quotes.java
@@ -1,8 +1,49 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class Quotes {
 
     /*Create a GUI program with three buttons.
     Each button displays one of your favorite quotes.
     Hint: Use \n to make a new line.
      */
+   public static void main(String[] args){
+       JFrame window = new JFrame("Quotes");
+       JPanel panel = new JPanel();
+       JButton quote1But = new JButton("Click for first quote");
+       JButton quote2But = new JButton("Click for second quote");
+       JButton quote3But = new JButton("Click for third quote");
 
+       window.setSize(400, 150);
+       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       window.setLocationRelativeTo(null);
+
+       quote1But.addActionListener(new QuoteBut1());
+       quote2But.addActionListener(new QuoteBut2());
+       quote3But.addActionListener(new QuoteBut3());
+
+       panel.add(quote1But);
+       panel.add(quote2But);
+       panel.add(quote3But);
+
+       window.add(panel);
+       window.setVisible(true);
+   }
+   private static class QuoteBut1 implements ActionListener {
+       public void actionPerformed (ActionEvent e){
+           JOptionPane.showMessageDialog(null, "\" This is my favorite quote\"\n-Me");
+
+       }
+   }
+   private static class QuoteBut2 implements ActionListener {
+          public void actionPerformed (ActionEvent e){
+              JOptionPane.showMessageDialog(null, "\" This is also my favorite quote\"\n-Me");
+
+   }
+}
+    private static class QuoteBut3 implements ActionListener {
+        public void actionPerformed (ActionEvent e) {
+            JOptionPane.showMessageDialog(null, "\" This isn't my favorite quote\"\n-Me");
+        }
+   }
 }
